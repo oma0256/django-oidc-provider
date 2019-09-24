013b387f2e9b4ee187f53134be624f1985ded453
Geoff V <ge.off.von@gmail.com>
2019-09-24 21:26:09 +0000

    Fix Login/outView imports in urls.py

--- example/app/urls.py
+++ example/app/urls.py
@@ -1,4 +1,11 @@
-from django.contrib.auth import views as auth_views
+try:
+    from django.contrib.auth.views import (
+        LogoutView, LoginView
+    )
+except ImportError:
+    from oidc_provider.compat import (
+        LogoutView, LoginView
+    )
 try:
     from django.urls import include, url
 except ImportError:
@@ -9,8 +16,8 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
-    url(r'^accounts/login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    url(r'^accounts/logout/$', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    url(r'^accounts/login/$', LoginView.as_view(template_name='login.html'), name='login'),
+    url(r'^accounts/logout/$', LogoutView.as_view(next_page='/'), name='logout'),
     url(r'^', include('oidc_provider.urls', namespace='oidc_provider')),
     url(r'^admin/', admin.site.urls),
 ]
